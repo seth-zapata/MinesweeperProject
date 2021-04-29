7400670e539f3f31fb343aff43f5ca67e72fcf86
Paul Perryman <79539673+Aie959@users.noreply.github.com>
2021-04-28 19:18:08 -0500

    Add files via upload

--- MPApp.java
+++ MPApp.java
@@ -4,12 +4,19 @@ package model;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.regex.Pattern;
+
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.text.Text;
 
 
 public class MPApp {
@@ -60,7 +67,7 @@ public class MPApp {
 	}
 	
 	// check if user entered a "username"
-	public static boolean checkIfUsernameEntered(String userName) {
+	public static boolean checkIfUsernameEntered(String userName) throws IOException {
 		// if no username is entered
 		if(userName.trim().isEmpty()) {
 			// display no "username" alert
@@ -71,22 +78,85 @@ public class MPApp {
 			a.show();
 			return true;
 		}
+		else if(Pattern.matches("[A-Z]{3}", userName) == false) {
+			Alert a = new Alert(AlertType.ERROR);
+			a.setTitle("Error");
+			a.setHeaderText("Invalid username format");
+			a.setContentText("Please enter 3 uppercase characters");
+			a.show();
+			return true;
+		
+		}
+		else if(Pattern.matches("[A-Z]{3}", userName)) {
+			File file=new File("userIDs.properties");
+			FileInputStream reader=new FileInputStream(file);
+			Properties properties=new Properties();
+			properties.load(reader);
+			reader.close();
+			if(properties.containsKey(userName)) {
+				Alert a = new Alert(AlertType.ERROR);
+				a.setTitle("Error");
+				a.setHeaderText("Username taken");
+				a.setContentText("Please enter new username");
+				a.show();
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		}
+		
 		return false;
 	}
 	
 	// add username to file
 	public static void addUserName(String username) throws IOException {
-		File file = new File("users.txt");
+		HashMap<String, String> hash=new HashMap<String,String>();
+		Properties properties=new Properties();
+		String val = getPassedValue();
+		String complete = String.format("%-14s%s", val, "20secs");
+		hash.put(username, complete);
+		properties.putAll(hash);
+		File file=new File("userIDs.properties");
+		FileOutputStream writer=new FileOutputStream(file,true);
+		properties.store(writer, null);
+		writer.close();
 
-		// if file doesn't exists make it  
-		if(!file.exists()) {
-			file.createNewFile();
-		}
-				
-		FileWriter writer = new FileWriter(file, true);
-			
-		// write username to file
-		writer.write(username + System.lineSeparator());
-        writer.close();
+	}
+
+	public static void updateScore(int score) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void stopTimerGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void addTime() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void setScene(Scene sceneGame) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void closeScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void getTime(Text time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static int getScore() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
